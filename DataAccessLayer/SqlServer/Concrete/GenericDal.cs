f434046e160fbe8268bc6eff8c9b4374642aeab8
mehribanaskerova <74506433+mehribanaskerova@users.noreply.github.com>
2023-01-06 16:19:23 +0400

    Добавьте файлы проекта.

---
+++ DataAccessLayer/SqlServer/Concrete/GenericDal.cs
@@ -0,0 +1,68 @@
+﻿using DataAccessLayer.SqlServer.Abstract;
+using DataAccessLayer.SqlServer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.SqlServer.Concrete
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T t)
+        {
+            using var c = new SqlServerContext();
+            c.Remove(t);
+            c.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            using var c = new SqlServerContext();
+            return c.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll(Expression<Func<T, bool>> filter = null)
+        {
+            using (var c = new SqlServerContext())
+            {
+                return filter == null ?
+                    c.Set<T>().ToList() :
+                c.Set<T>().Where(filter).ToList();
+            }
+        }
+        //public List<T> GetListAll(Expression<Func<T, bool>> filter)
+        //{
+        //    using var c = new SqlServerContext();
+        //    return c.Set<T>().Where(filter).ToList();
+        //}
+
+        public void Insert(T t)
+        {
+            using var c = new SqlServerContext();
+            c.Add(t);
+            c.SaveChanges();
+        }
+
+        public T GetByFilter(Expression<Func<T, bool>> filter = null)
+        {
+            using var c = new SqlServerContext();
+            if (filter == null)
+                return c.Set<T>().FirstOrDefault();
+            else
+                return c.Set<T>().FirstOrDefault(filter);
+        }
+
+
+        public void Update(T t)
+        {
+            using var c = new SqlServerContext();
+            c.Update(t);
+            c.SaveChanges();
+        }
+
+       
+    }
+}
---
+++ DataAccessLayer/SqlServer/Context/SqlServerContext.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.SqlServer.Context
+{
+    public class SqlServerContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Server=USER-NUR ;Database=CoreDB;Integrated security=True;");
+        }
+
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
