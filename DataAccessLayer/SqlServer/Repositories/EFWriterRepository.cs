8309431ef3f933d74fff2fc03960cd6932df3377
mehribanaskerova <74506433+mehribanaskerova@users.noreply.github.com>
2023-01-07 18:26:43 +0400

    20

---
+++ DataAccessLayer/SqlServer/Repositories/EFWriterRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.SqlServer.Abstract;
+using DataAccessLayer.SqlServer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.SqlServer.Repositories
+{
+    public class EFWriterRepository : GenericRepository<Writer>, IWriterDal
+    {
+    }
+}
