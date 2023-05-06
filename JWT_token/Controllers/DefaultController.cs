b658ef044919cdb95959c4b65b3c3df6e6fff8df
mehribanaskerova <74506433+mehribanaskerova@users.noreply.github.com>
2023-05-06 23:49:14 +0500

    JWT token

---
+++ JWT_token/Controllers/DefaultController.cs
@@ -0,0 +1,31 @@
+﻿using JWT_token.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWT_token.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+       
+            [HttpGet("[action]")]
+            public IActionResult Login()
+            {
+                return Created("", new BuiltToken().CreateToken());
+            }
+
+            [Authorize]
+            [HttpGet("[action]")]
+            public IActionResult Page1()
+            {
+                return Ok("Sayfa 1 İçin Girişiniz Başarılı");
+            }
+        
+    }
+}
